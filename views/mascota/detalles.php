370b462569e9e2f2bf3dba38080a8b7fb235ae82
sergio <lopez25bcn@gmail.com>
2020-03-10 19:07:27 +0100

    Ultima version del projecto 10 de marzo 19:07

--- views/mascota/detalles.php
+++ views/mascota/detalles.php
@@ -14,6 +14,14 @@
 		
 		<h2>Detalles del mascota</h2>
 		<h3><?="$mascota->nombre $mascota->fechafallecimiento"?></h3>
+		<figure class="portada">
+		<?php 
+		echo $mascota->imagen ?
+		"<img height='30' src='/$mascota->imagen' alt='$mascota->nombre'>":
+		"<img height='30' src=/imagenes/mascotas/default.png' alt='$mascota->nombre'>";
+		
+		?>
+		</figure>
 		
 		<p><b>Nombre:</b> <?=$mascota->nombre?></p>
 		<p><b>Sexo:</b> <?=$mascota->sexo?></p>
@@ -22,10 +30,12 @@
        	<p><b>Fecha Fallecimiento</b> <?=$mascota->fechafallecimiento?></p>
 		
 	
-	
+		<?php if(Login::isAdmin() || Login::hasPrivilege(500)||Login::get()->id==$mascota->idusuario){?>
 		<a href="/mascota/edit/<?=$mascota->id?>">Editar mascota</a> - 
 		<a href="/mascota/delete/<?=$mascota->id?>">Borrar mascota</a> - 
+		<?php }?>
 		<a href="/mascota/list">Lista de mascota</a> 
+		
 			
 	
     	<form action="/foto/store">
--- views/mascota/lista.php
+++ views/mascota/lista.php
@@ -23,13 +23,20 @@
 			</tr>
     		<?php foreach($mascotas as $mascota){
     			   echo "<tr>";   
+    			   echo "<td>";
+    			   echo $mascota->imagen ?
+    			   "<img height='30' src='/$mascota->imagen' alt='$mascota->nombre'>":
+    			   "<img height='30' src=/imagenes/mascotas/default.png' alt='$mascota->nombre'>";
     			   echo "<td>$mascota->nombre</td>";
     			   echo "<td>$mascota->sexo</td>";
     			   echo "<td>$mascota->biografia</td>";
     			   echo "<td>$mascota->fechanacimiento</td>";
+    			   //echo "<td>$mascota->idusuario</td>";
     			   echo "<td>";
     			   echo " <a href='/mascota/show/$mascota->id'>Ver</a>";
-    			   if(Login::isAdmin() || Login::hasPrivilege(500)){
+    			   
+    			   if(Login::isAdmin() || Login::hasPrivilege(500)||Login::get()->id==$mascota->idusuario){
+    			       
     			   echo "-<a href='/mascota/edit/$mascota->id'>Actualizar</a>";
     			   echo "-<a href='/mascota/delete/$mascota->id'>Borrar</a>";
     			   }
